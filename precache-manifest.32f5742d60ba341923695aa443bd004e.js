649f4dfd298a424c10532f6dfd0ca28627cdaa15
Mikhail Probochkin <mikhailprobochkin@mikhail-mac.local>
2019-11-19 22:25:29 +0300

    Updates

---
+++ precache-manifest.32f5742d60ba341923695aa443bd004e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c06fd8367b9c1377b94cafe4559406c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "11e33f9a34323d5e61bf",
+    "url": "./static/css/main.ae67e342.chunk.css"
+  },
+  {
+    "revision": "eefaa6a62e0f28428bd7",
+    "url": "./static/js/2.ca7e7301.chunk.js"
+  },
+  {
+    "revision": "11e33f9a34323d5e61bf",
+    "url": "./static/js/main.a53939db.chunk.js"
+  },
+  {
+    "revision": "07a5fd76a3cd37e93294",
+    "url": "./static/js/runtime-main.7d629ed2.js"
+  }
+]);
